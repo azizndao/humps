04b2cbc726c5f4e16f147e4d234e3e9b0a7a36a2
Abdou Aziz Ndao <azizndao71@gmail.com>
2024-01-11 20:48:56 +0000

    update the documentation

--- lib/src/humps.dart
+++ lib/src/humps.dart
@@ -1,3 +1,8 @@
+/// Converts the keys of a [Map] from snake case to camel case.
+///
+/// Iterates through all key-value pairs in the map, converting each key to
+/// camel case. For nested map values that are themselves maps, recursively
+/// converts their keys to camel case as well.
 extension MapCamelizeKeys on Map<String, dynamic> {
   /// Converts this JSON map's keys to camel case.
   ///
@@ -127,7 +132,12 @@ extension MapCamelizeKeys on Map<String, dynamic> {
   }
 }
 
+/// Extends the [List] class with additional methods.
+///
+/// This extension adds utility methods to make it easier to work with [List]s.
 extension ListExtensions<E> on List<E> {
+  /// Iterates over the elements of the list, passing each element and its
+  /// index to the given [convert] function, and yields the result.
   Iterable<R> _mapIndexed<R>(R Function(int index, E element) convert) sync* {
     for (var index = 0; index < length; index++) {
       yield convert(index, this[index]);
@@ -135,7 +145,12 @@ extension ListExtensions<E> on List<E> {
   }
 }
 
+/// Extends the [String] class with additional methods.
+///
+/// This extension adds utility methods to make it easier to work with [String]s.
 extension StringX on String {
+  /// Converts the string to capital case by uppercasing the first letter
+  /// and lowercasing the rest of the string.
   String toCapitalCase() {
     return '${this[0].toUpperCase()}${substring(1)}';
   }
